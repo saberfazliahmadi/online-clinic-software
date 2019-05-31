71fda7fd6ea3e62020e96af5804c9ed601610362
Saber <saberfazliahmadi@gmail.com>
2019-05-31 10:18:00 -0700

    php objective example on git hub

--- matab2.php
+++ matab2.php
@@ -2,7 +2,8 @@
 echo
 'This is matab 1.php
 <br>
-'.time().'
+the DATE is :
+'.date('Y-m-d').'
 <br>
 it is version 1
 <br>
@@ -11,16 +12,37 @@ it is a new section by web editor on cloud repository' ;
 /**
  *
  */
+
+// echo $date = date('Y-m-d') ;
+
+// lets define a class which name is login.
 class login
 {
   public $user_name = 'saber' ;
-  if($user_name == 'saber')
+  public $password  = '123456' ;
+
+  public function setProperty($newval)
   {
-    echo 'yes this is SABER' ;
+      $this->user_name = $newval;
+  }
+
+  public function getProperty()
+  {
+      return $this->user_name.'<br/>' ;
   }
 }
 
+// lets define an object.
 $obj = new login ;
-var_dump($obj) ;
 
+// lets call our object.
+echo '<br/>'.$obj->user_name ;
+echo '<br/>'.$obj->password.'<br/>' ;
+
+echo $obj->getProperty(); // Get the property value
+
+$obj->setProperty("SABER FAZLI AHMADI"); // Set a new one
+
+echo $obj->getProperty(); // Read it out again to show the change
+echo '<br/>' ;
 ?>
