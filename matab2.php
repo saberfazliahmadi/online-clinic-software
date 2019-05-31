481f6b4f38f5547da12e0717453a41d1500cf938
Saber <saberfazliahmadi@gmail.com>
2019-05-31 04:26:00 -0700

    let check the laravel with git

--- matab2.php
+++ matab2.php
@@ -7,4 +7,14 @@ echo
 it is version 1
 <br>
 it is a new section by web editor on cloud repository' ;
+
+/**
+ *
+ */
+class login
+{
+  public $user_name = 'saber' ;
+}
+$obj = new login ;
+var_dump($obj) ;
 ?>
